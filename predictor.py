2602ecfb73730ad48bff5bbca4de47a6eb12a93d
NetoPedro <pedroneto_09@hotmail.com>
2018-08-16 17:36:32 +0300

    Defined Pipeline sequence and applied it to the data

--- predictor.py
+++ predictor.py
@@ -10,6 +10,15 @@ print(train_set.info())
 print(train_set.describe())
 from sklearn.base import BaseEstimator, TransformerMixin
 
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attribute_names]
+
 class CreateDistance(BaseEstimator,TransformerMixin):
 
     def __init__(self):pass
@@ -23,7 +32,6 @@ class CreateDistance(BaseEstimator,TransformerMixin):
         # approximate radius of earth in km
         R = 6373.0
 
-
         dlon = X["dropoff_longitude"].values - X["pickup_longitude"].values
         dlat = X["dropoff_latitude"].values - X["pickup_latitude"].values
 
@@ -32,7 +40,7 @@ class CreateDistance(BaseEstimator,TransformerMixin):
 
         X["distance"] = R * c
         X = X.drop(["pickup_latitude","pickup_longitude","dropoff_longitude","dropoff_latitude"],axis=1)
-        return X
+        return np.c_[X]
 
 class CategorizePickupTime(BaseEstimator,TransformerMixin):
     def __init__(self):pass
@@ -65,7 +73,7 @@ class CategorizePickupTime(BaseEstimator,TransformerMixin):
                 X.loc[X["pickup_datetime"] == int(entry[0]), "pickup_datetime"] = -2
 
         X["pickup_datetime"] = X["pickup_datetime"] * -1
-        return X
+        return np.c_[X]
 
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler
@@ -73,49 +81,31 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.pipeline import FeatureUnion
 pipeline_num = Pipeline([
-    ("imputer", Imputer(strategy="mean")),
+    ("selector",DataFrameSelector(["pickup_longitude","pickup_latitude","dropoff_longitude","dropoff_latitude","passenger_count"])),
     ("distance_creator",CreateDistance()),
+    ("imputer", Imputer(strategy="mean")),
     ("scaler", StandardScaler())
 ])
 
 pipeline_cat = Pipeline([
+    ("selector", DataFrameSelector(["pickup_datetime"])),
     ("time_category_creator",CategorizePickupTime()),
-    ("HotEncoder",OneHotEncoder())
+    ("HotEncoder",OneHotEncoder(sparse=False))
 ])
 
 final_pipeline = FeatureUnion([
-    ("num_pipeline", pipeline_num),
-    ("cat_pipelone",pipeline_cat)
+("cat_pipelone",pipeline_cat),
+    ("num_pipeline", pipeline_num)
+
 ])
 
+y_train = train_set.drop(["key","pickup_datetime","pickup_longitude","pickup_latitude","dropoff_longitude","dropoff_latitude","passenger_count"],axis=1)
+X_train = train_set.drop(["key","fare_amount"],axis=1)
+X_train = train_set.append(test_set.drop("key",axis=1))
+
+X_train = final_pipeline.fit_transform(X_train)
 
 
-'''
-train_set["pickup_datetime"] = (train_set["pickup_datetime"].str.extract('( ..)', expand=True).astype(int))
-by_hour = train_set["pickup_datetime"].value_counts()
+X_test = X_train[chunksize:]
+X_train = X_train[:chunksize]
 
-mean = np.mean(by_hour)
-std = np.std(by_hour)
-
-by_hour = train_set["pickup_datetime"].value_counts().__str__()
-by_hour = by_hour.split("\n")
-i = 0
-for string in by_hour:
-    by_hour[i]= (re.findall(r'\S+', by_hour[i]))
-    i +=1
-
-by_hour = np.transpose(by_hour)
-
-for entry in by_hour:
-    if len(entry)!=2: continue
-    if int(entry[1]) < mean - std:
-        train_set.loc[train_set["pickup_datetime"] == int(entry[0]),"pickup_datetime"] = -1
-    elif int(entry[1]) > mean + std:
-        train_set.loc[train_set["pickup_datetime"] == int(entry[0]),"pickup_datetime"] = -3
-    else: train_set.loc[train_set["pickup_datetime"] == int(entry[0]),"pickup_datetime"] = -2
-
-
-train_set["pickup_datetime"] = train_set["pickup_datetime"] * -1
-'''
-# TODO move to class
-# TODO Pipeline
